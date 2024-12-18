bae5e9264d27fd5cf348eea714bf0f7f18b214b5
unknown <larris.xie@gmail.com>
2024-12-17 23:28:33 -0500

    Updated resume viewing

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -5,21 +5,26 @@ import Head from "next/head";
 const index = () => {
   return (
     <>
-    <Head>
+      <Head>
         <title>Larris&apos;s Resume</title>
         <meta name="description" content="Larris Xie's Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo.png" type="image/png"></link>
       </Head>
-    <main className={styles.wrapper}>
-      <object
-        data="/resume.pdf"
-        type="application/pdf"
-        width="100%"
-        height="100%"
-      >
-      </object>
-    </main>
+      <main className={styles.wrapper}>
+        <object
+          data="resume.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+        >
+          <embed
+            src="./resume.pdf"
+            width="100%"
+            height="100%"
+          />
+        </object>
+      </main>
     </>
   );
 };
